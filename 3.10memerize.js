5352a4a95d52eb1601dae116ef4212d4d9ab4c3c
xiaohorse <1778911348@qq.com>
2024-03-10 14:23:09 +0800

    feat: 之前算法题温习

--- 3.10memerize.js
+++ 3.10memerize.js
@@ -81,3 +81,121 @@ function twoSum(nums, target) {
 }
 
 console.log(twoSum([2, 3, 4, 7], 10))
+
+
+/**
+ * 最多删除一个字符是否是 panlindorme（回文）
+ * @param {*} s 
+ * @returns 
+ */
+function delOneIsPalindrom(s) {
+
+    let left = 0, right = s.length - 1;
+
+    while (left <= right) {
+        if (s[left] === s[right]) {
+            left++;
+            right--;
+        } else {
+            if (isPalindrom(left + 1, right, s) || isPalindrom(left, right - 1, s)) {
+                return true
+            } else {
+                return false
+            }
+        }
+
+    }
+
+    function isPalindrom(left, right, s) {
+
+        while (left <= right) {
+            if (s[left] !== s[right]) {
+                return false
+            } else {
+                left++;
+                right--;
+            }
+        }
+
+        return true
+    }
+
+    return true
+}
+
+console.log(delOneIsPalindrom('aba'));
+console.log(delOneIsPalindrom('abcca'));
+
+/**
+ * 合并两个有序链表
+ */
+function mergeL(l1, l2) {
+
+    if (!l1 || !l2) return l1 || l2;
+
+    let head = cur = {
+        val: 'head',
+        next: null
+    }
+
+    while (l1 && l2) {
+        if (l1.val < l2.val) {
+            cur.next = l1;
+            l1 = l1.next;
+        } else {
+            cur.next = l2;
+            l2 = l2.next;
+        }
+
+        cur = cur.next;
+    }
+
+    cur.next = l1 || l2;
+    return head.next
+}
+
+// 链表删除操作
+
+
+/**
+ * 给定一个排序链表，删除所有重复的元素，使得每个元素只出现一次。这题这次复习做错了，attention！(当前重复的节点保留一个)
+ */
+function delNode(l) {
+
+    let cur = l;
+
+    while (cur && cur.next) {
+
+        if (cur.val === cur.next.val) {
+            cur.next = cur.next.next;
+        } else {
+            cur = cur.next;
+        }
+    }
+
+    return l;
+
+}
+
+
+// 重复的元素所有的都删除
+function delAllMulti(l) {
+    let head = {
+        next: l
+    }
+
+    cur = head;
+
+    while (cur.next && cur.next.next) {
+        if (cur.next.val === cur.next.next.val) {
+            const value = cur.next.val;
+            while (value === cur.next.value) {// 这里缺少了个 cur.next !== null判断
+                cur.next = cur.next.next;
+            }
+        } else {
+            cur = cur.next
+        }
+    }
+
+    return head.next;
+}
